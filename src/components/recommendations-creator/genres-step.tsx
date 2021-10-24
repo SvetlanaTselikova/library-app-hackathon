040758508dabbb849063dfc94147961c309a1c36
Svetlana.Tselikova <Svetlana.Tselikova@nordigy.ru>
2021-10-24 22:56:48 +0600

    limit genres

--- src/components/recommendations-creator/genres-step.tsx
+++ src/components/recommendations-creator/genres-step.tsx
@@ -1,8 +1,6 @@
-import React from "react";
-import { Tag, Skeleton, Select } from "antd";
+import React, { useState } from "react";
+import { Skeleton, Select } from "antd";
 import styles from "./index.module.sass";
-
-const { CheckableTag } = Tag;
 
 type Props = {
   isLoadingGenres: boolean;
@@ -13,16 +11,21 @@ type Props = {
 
 export const GenresStep = (props: Props) => {
   const { isLoadingGenres, genresData, selectedGenres, onChange } = props;
+  const [optionsSelected, setOptionsSelected] = useState<string[]>([]);
+  const handleChange = (value) => {
+    setOptionsSelected(value);
+    onChange(value);
+  };
   return isLoadingGenres ? (
     <Skeleton active />
   ) : (
     <div className={styles.genresContent}>
+      <div className={styles.helpText}>*Вы можете выбрать до 7-ми жанров</div>
       <Select
         placeholder="Выберите жанры..."
-        onChange={onChange}
+        onChange={handleChange}
         mode="multiple"
         className={styles.genresSelect}
-        maxTagCount={6}
         loading={isLoadingGenres}
         value={selectedGenres as any}
       >
@@ -31,7 +34,17 @@ export const GenresStep = (props: Props) => {
               .slice()
               .sort((one, two) => one.localeCompare(two))
               .map((item, inx) => (
-                <Select.Option key={inx} value={item}>
+                <Select.Option
+                  key={inx}
+                  value={item}
+                  disabled={
+                    optionsSelected.length > 6
+                      ? optionsSelected.includes(item)
+                        ? false
+                        : true
+                      : false
+                  }
+                >
                   {item}
                 </Select.Option>
               ))
